04be833019e32a2903d2a38fd2b21a84bb82a847
Ricky Yuanqi Li <yuanqili@umail.ucsb.edu>
2017-11-04 13:54:12 -0700

    sleep when exceed rate limit

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -1,10 +1,13 @@
-import json
 from utilities import print_json
 from AppAuthClient import Client
 from db import DBManager, Tweet
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import func
+
+from time import localtime, strftime
+import pause
+from datetime import datetime
 
 CONSUMER_KEY = 'GcVofuJBUpOv6LKLLkHf9Y79e'
 CONSUMER_SECRET = 'Sv7W2KEWePjJqIeKwsj7QV4hxU0Svux0WxGm70DbfCktTqQMqI'
@@ -16,10 +19,21 @@ if __name__ == '__main__':
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
     db = DBManager(DB_PATH)
 
-    for i in range(100):
-        print('query {}'.format(i))
+    i = 0
+    while 1:
+        i += 1
+        print('query: {}'.format(i))
         min_id = db.session.query(func.min(Tweet.id)).scalar() - 1
         tweets = client.search_tweets(q='apple', count=100, result_type='recent', max_id=min_id)
+
+        if 'statuses' not in tweets:
+            limit = client.rate_limit_status()
+            reset = int(limit['resources']['search']['/search/tweets']['reset'])
+            print('sleep until {}'.format(strftime("%a, %d %b %Y %H:%M:%S", localtime(reset))))
+            # pause.until(reset)
+            pause.minutes(2)
+            continue
+
         for tweet in tweets['statuses']:
             try:
                 db.add(Tweet(id=tweet['id'],
