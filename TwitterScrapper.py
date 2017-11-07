2365382f15e24f1cb06aaf9f9434e980fd9a55d4
Ricky Yuanqi Li <yuanqili@umail.ucsb.edu>
2017-11-06 18:25:54 -0800

    improve readability

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -1,31 +1,32 @@
-from utilities import print_json
+from time import localtime, strftime
+
+import pause
+from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
+
 from AppAuthClient import Client
 from db import DBManager, Tweet
-
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy import func
-
-from time import localtime, strftime
-import pause
-from datetime import datetime
 
 CONSUMER_KEY = 'GcVofuJBUpOv6LKLLkHf9Y79e'
 CONSUMER_SECRET = 'Sv7W2KEWePjJqIeKwsj7QV4hxU0Svux0WxGm70DbfCktTqQMqI'
 
-DB_PATH = '/Users/yuanqili/Development/TwitterScrapper/tweets.sqlite'
+DB_PATH = '/path/to/db/db-name.sqlite'
 
 
-if __name__ == '__main__':
+def tweets_search(query):
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
     db = DBManager(DB_PATH)
 
     i = 0
-    while 1:
+    while True:
         i += 1
         print('query: {}'.format(i))
-        min_id = db.session.query(func.min(Tweet.id)).scalar() - 1
-        tweets = client.search_tweets(q='apple', count=100, result_type='recent', max_id=min_id)
+        min_id = db.session.query(func.min(Tweet.id)).scalar()
+        min_id = min_id - 1 if min_id else None
+        tweets = client.search_tweets(q=query, result_type='recent', lang='en', max_id=min_id)
 
+        # If exceeds rate limit, wait until reset
+        # Seems not working properly, to be fixed
         if 'statuses' not in tweets:
             limit = client.rate_limit_status()
             reset = int(limit['resources']['search']['/search/tweets']['reset'])
@@ -34,15 +35,14 @@ if __name__ == '__main__':
             pause.minutes(2)
             continue
 
+        # Adds tweets into database
         for tweet in tweets['statuses']:
             try:
-                db.add(Tweet(id=tweet['id'],
-                             created_at=tweet['created_at'],
-                             text=tweet['text'],
-                             meta_data=str(tweet)))
+                db.add(Tweet.parse(tweet))
             except IntegrityError as err:
                 print(err)
                 db.rollback()
 
-    status = client.rate_limit_status()
-    print_json(status)
+
+if __name__ == '__main__':
+    tweets_search('apple')
